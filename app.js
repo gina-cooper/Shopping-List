f2de284a8d73f5ccf946a81db1eef1b19822dbde
Gina Cooper <gcooper2@devry.edu>
2022-01-11 16:03:16 -0500

    Module 5

--- app.js
+++ app.js
@@ -11,26 +11,66 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 const items = []; //Array of all items on shopping list
+const bulkItems = []; //Array of bulk items
+
 //Create a root route for the get method
 app.get('/', function(req,res) {
    
-  res.render("list", {listItems: items}); //send the items array to the list.ejs page
+  res.render("list", {listName:"Shopping List", listItems: items}); //send the items array to the list.ejs page
+  });
+
+  //bulk items route
+  app.get("/bulk", function(req,res){
+    res.render("list", {listName: "Bulk Items", listItems: bulkItems});
   });
 
   //retrieve data and display it on the screen and to the console
   app.post("/", function(req, res){
+    
+    const query = req.query.newItem; //retrieve item from querystring
+    console.log(query);
 
     const item = req.body.newItem; //retrieve item from form named newItem
-    items.push(item); //add new item to the list  
-    res.redirect("/");
+    const list = req.body.list; //retrieve item from list
+    if (typeof(query) != "undefined") //check if they querystring is not blank
+    {
+
+      items.push(query); //add new item to the list  
+      res.redirect("/");
+    }
+    if(typeof(item) != "undefined") //check if the item from the form is not blank
+    {
+      if (list === "Shopping List") //Check the name of the list
+      {
+        items.push(item);
+    
+      res.redirect("/");
+      }
+      else //assume if the list is not Shopping list it is bulk items. If more routes are added  
+      {    //this will need to be an else if statement
+        bulkItems.push(item);
+        res.redirect("/bulk");
+      }
+    }
+
   });
   
   app.post("/delete", function(req,res)
   {
     
   const checkedItemId = req.body.checkbox;//Retrieve the checked item
-  items.splice(checkedItemId,1); //delete the checked item from the array
-  res.redirect("/"); //redirect back to the home page and render the list
+  const list = req.body.thislist; //Retrieve the name of the list
+
+if (list === "Shopping List")
+  {
+    items.splice(checkedItemId,1);
+	  res.redirect("/");
+  }
+  else 
+  {
+    bulkItems.splice(checkedItemId,1);
+    res.redirect("/bulk");
+  }
   });
   
 
